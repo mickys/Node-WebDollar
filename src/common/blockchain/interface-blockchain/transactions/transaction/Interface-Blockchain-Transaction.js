6089cf9ec6e7f69e96f7a2b94749940db9597384
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-09-15 22:34:52 +0300

    avoid saving tx serialized

--- src/common/blockchain/interface-blockchain/transactions/transaction/Interface-Blockchain-Transaction.js
+++ src/common/blockchain/interface-blockchain/transactions/transaction/Interface-Blockchain-Transaction.js
@@ -267,10 +267,12 @@ class InterfaceBlockchainTransaction{
 
     serializeTransaction(rewrite = false){
 
-        if ( !this._serializated || rewrite )
-            this._serializated = this._serializeTransaction();
+        return this._serializeTransaction();
 
-        return this._serializated;
+        // if ( !this._serializated || rewrite )
+        //     this._serializated = this._serializeTransaction();
+        //
+        // return this._serializated;
 
     }
 
